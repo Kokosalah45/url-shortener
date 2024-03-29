aba35c956880853e4e0952a566f50fd7d8dc3ac7
Kerolous Amged <kerolous.amged@nagwa.com>
2024-03-29 22:30:31 +0200

    feat: routes now load in memory

--- main.go
+++ main.go
@@ -1,59 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
 
-
-func ReadAll(r io.Reader) ([]byte , error){
-
-	buffer := make([]byte , 100)
-	
-	for {
-		tempBuffer := make([]byte , 50)
-		_ , err := r.Read(tempBuffer)
-		if  err != nil {
-			if err == io.EOF{
-				return buffer , io.EOF
-			}
-			
-			return  nil, err
-		}
-		buffer = append(buffer , tempBuffer...)
-
-	}
-	
+type Route struct {
+	Path string `yaml:"path" json:"path"`
+	URL  string `yaml:"url" json:"url"`
 }
 
+type Routes = []Route
 
+func ReadAll(r io.Reader) ([]byte, error) {
+	buffer := make([]byte, 0)
 
-func main (){
+	for {
+		tempBuffer := make([]byte, 1024)
+		n, err := r.Read(tempBuffer)
+		if err != nil {
+			if err == io.EOF {
+				return buffer, io.EOF
+			}
+			return nil, err
+		}
+		buffer = append(buffer, tempBuffer[:n]...)
+	}
+}
 
-	sourcePtr := flag.String("source" , "" , "path of file to be parsed")
+func main() {
+	sourcePtr := flag.String("source", "", "path of file to be parsed")
 	flag.Parse()
 
 	filePath := *sourcePtr
 
+	file, err := os.Open(filePath)
 
-	file , err := os.Open(filePath)
 	fileType := filepath.Ext(filePath)
-	fmt.Println(fileType)
-
 
 	if err != nil {
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-
-	if byteSLice , err := ReadAll(file); len(byteSLice) != 0 && err == io.EOF{
-		fmt.Println(string(byteSLice))
-	}
-
 	defer file.Close()
 
+	var data Routes
+	if byteSlice, err := ReadAll(file); err == io.EOF && len(byteSlice) != 0 {
+		if fileType == ".yaml" {
+			if err := yaml.Unmarshal(byteSlice, &data); err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+		}
+		if fileType == ".json" {
+			if err := json.Unmarshal(byteSlice, &data); err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+
+		}
+	}
+	fmt.Println(data)
 
 }
-
